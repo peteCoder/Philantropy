b5122ba28a7781905de1ff435808c93d42511bf4
Peter <talk2peteresezobor@gmil.com>
2020-11-30 19:15:53 +0100

    my app second commit

--- GACI/settings.py
+++ GACI/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =[] #['giftachildapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS =['giftachildapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
