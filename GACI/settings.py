7ea8fca48ac5b822767156448fb662ddc825489e
Peter <talk2peteresezobor@gmil.com>
2020-12-01 18:19:54 +0100

    staticfiles commit

--- GACI/settings.py
+++ GACI/settings.py
@@ -175,7 +175,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
