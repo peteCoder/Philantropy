d5d764a57ea5755eca2c130378d0f830479e4b84
Peter <talk2peteresezobor@gmil.com>
2020-12-01 21:29:09 +0100

    update my heroku app_name

--- GACI/settings.py
+++ GACI/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['giftachildapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['mygiftachildinitiativeapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
