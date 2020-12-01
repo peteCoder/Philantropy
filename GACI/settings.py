9d72110dcc3c38ad2265656ab4e7d21409b41a58
Peter <talk2peteresezobor@gmil.com>
2020-12-01 18:02:06 +0100

    static file commit

--- GACI/settings.py
+++ GACI/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [] #['giftachildapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['giftachildapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -173,14 +173,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
